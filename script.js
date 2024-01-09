448470284da6355d8d24d99022b01dc267f76091
njenkins2727 <njenkins2727@gmail.com>
2024-01-09 15:33:56 +0800

    added login and register form, need to work on functionality. also added a save-recipe button on cards in catergories

---
+++ script.js
@@ -0,0 +1,56 @@
+//get items 
+// const fullName = document.getElementById('name-reg')
+// const email = document.getElementById('email-reg')
+// const password = document.getElementById('password-reg')
+// const signUpButton = document.getElementById('sign-up-reg')
+// const msgDiv = document.getElementsByClassName("message");
+
+// function displayMessage(type, message) {
+//     msgDiv.textContent = message;
+//     msgDiv.setAttribute("class", type);// this is the issue, set attribute 
+//     // console.log(type)
+//     // console.log(message)
+//   }
+  
+
+// signUpButton.addEventListener('click', function (event){
+//     event.preventDefault();
+
+    
+//     const fullName = document.getElementById('name-reg').value
+//     const email = document.getElementById('email-reg').value
+//     const password = document.getElementById('password-reg').value
+    
+//     if(fullName === ""){
+//     displayMessage("error", "Name cannot be blank");
+//     }  if (email === "") {
+//     displayMessage("error", "Email cannot be blank");
+//   } else if (password === "") {
+//     displayMessage("error", "Password cannot be blank");
+//   } else {
+//    return displayMessage("success", "Registered successfully");
+//   }
+
+//     localStorage.setItem("name", fullName);
+//     localStorage.setItem("email", email);
+//     localStorage.setItem("password", password);
+
+
+// })
+
+//REGISTER
+// when i dont fill in field, then i am shown that it is required 
+// when i fill credentials and click button, then the crediendtails get verifyed to see if it has already been signed up 
+// when i enter credientialsand click sign up button, then form disappears and i am shown message sign up successful
+// when i sign up then i am added to the local storage 
+
+//LOGIN
+// when i click register then i am taken to register page 
+// when i fill in email and password and click login, credentials are pulled out from local storage  
+// when i get info from local storage then i display the full name in the "saved" catgory 
+// when i get local sotrage, then i display saved recipes in the saved category
+// when i cick login with no credentials, then i return error message 
+// when i return to home page logged in, then i see a sign out button f
+
+// when i click save recipe button on a recipe, then change "save" to "saved!" or have a bookmark icon and fill with color after clicked to represent saved??
+
