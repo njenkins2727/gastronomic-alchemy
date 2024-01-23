bdbb28c5be7859e8cfe962da033d46cce01d8e7f
njenkins2727 <njenkins2727@gmail.com>
2024-01-23 16:25:33 +0800

    full name email and password can save to local storage. need to continue functionality

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-//get items 
-// const fullName = document.getElementById('name-reg')
-// const email = document.getElementById('email-reg')
-// const password = document.getElementById('password-reg')
-// const signUpButton = document.getElementById('sign-up-reg')
-// const msgDiv = document.getElementsByClassName("message");
+// get items 
+const fullName = document.getElementById('name-reg')
+const email = document.getElementById('email-reg')
+const password = document.getElementById('password-reg')
+const signUpButton = document.getElementById('sign-up-reg')
+const msgDiv = document.getElementsByClassName("message");
 
 // function displayMessage(type, message) {
 //     msgDiv.textContent = message;
@@ -13,13 +13,13 @@
 //   }
   
 
-// signUpButton.addEventListener('click', function (event){
-//     event.preventDefault();
+signUpButton.addEventListener('click', function (event){
+    event.preventDefault();
 
     
-//     const fullName = document.getElementById('name-reg').value
-//     const email = document.getElementById('email-reg').value
-//     const password = document.getElementById('password-reg').value
+    const fullName = document.getElementById('name-reg').value
+    const email = document.getElementById('email-reg').value
+    const password = document.getElementById('password-reg').value
     
 //     if(fullName === ""){
 //     displayMessage("error", "Name cannot be blank");
@@ -31,20 +31,20 @@
 //    return displayMessage("success", "Registered successfully");
 //   }
 
-//     localStorage.setItem("name", fullName);
-//     localStorage.setItem("email", email);
-//     localStorage.setItem("password", password);
+    localStorage.setItem("name", fullName);
+    localStorage.setItem("email", email);
+    localStorage.setItem("password", password);
 
 
-// })
+})
 
-//REGISTER
+// REGISTER
 // when i dont fill in field, then i am shown that it is required 
 // when i fill credentials and click button, then the crediendtails get verifyed to see if it has already been signed up 
 // when i enter credientialsand click sign up button, then form disappears and i am shown message sign up successful
 // when i sign up then i am added to the local storage 
 
-//LOGIN
+// LOGIN
 // when i click register then i am taken to register page 
 // when i fill in email and password and click login, credentials are pulled out from local storage  
 // when i get info from local storage then i display the full name in the "saved" catgory 
